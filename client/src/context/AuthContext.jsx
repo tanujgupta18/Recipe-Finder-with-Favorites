85af9e5f2506a775d1c75271d907cd9fdaef0946
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-03-26 23:43:34 +0530

    Context Defined

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,54 @@
+import React, { createContext, useEffect, useState } from "react";
+
+const parseJwt = (token) => {
+  try {
+    return JSON.parse(atob(token.split(".")[1]));
+  } catch (error) {
+    return null;
+  }
+};
+
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(second);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedUser = parseJwt(token);
+
+      if (decodedUser && decodedUser.exp * 1000 > Date.now()) {
+        setUser({
+          id: decodedUser.id,
+          name: decodedUser.name,
+          email: decodedUser.email,
+        });
+      } else {
+        localStorage.removeItem("token");
+      }
+    }
+    setLoading(false);
+  }, []);
+
+  const login = () => {
+    setUser(null);
+    localStorage.removeItem("token");
+  };
+
+  const authContextValue = {
+    user,
+    loading,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
